79bdb561e97cfee80aedb8209909f15a63a10e17
jornosrichard090 <jornosrichard090@gmail.com>
2025-01-29 00:20:02 +0530

    Updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,17 +6,18 @@ import instagram_icon from "../assets/icons/instagram.png";
 export default function Footer() {
   return (
     <div className='bg-gradient-to-r py-4 from-pink-600 to-blue-600 text-center text-white'>
-        
-        
-        <a href="mailto:aashishgulshan@gmail.com"> aashishgulshan@gmail.com</a>
-            <div className='flex gap-2 justify-center items-center'>
-            <a href="https://www.linkedin.com/in/aashishgulshan/" target="_blank"> <img src={linkedin_icon} width={35} alt="" /></a>
-            <a href="https://www.facebook.com/aashishgulshan" target="_blank"> <img src={Facebook_icon} width={35} alt="" /></a>
-            <a href="https://github.com/aashishgulshan" target="_blank"><img src={github_icon} width={35} alt="" /></a>
-            <a href="https://www.instagram.com/aashish_gulshan/" target="_blank"><img src={instagram_icon} width={35} alt="" /></a>
-            </div>
-        <p>©2023 by Aashish Gulshan. Made with ❤️ in India</p>
-    
+
+
+      {/* <a href="mailto:aashishgulshan@gmail.com"> aashishgulshan@gmail.com</a> */}
+      <div className='flex gap-2 justify-center items-center'>
+        {/* <a href="https://www.linkedin.com/in/aashishgulshan/" target="_blank"> <img src={linkedin_icon} width={35} alt="" /></a> */}
+
+        {/* <a href="https://github.com/aashishgulshan" target="_blank"><img src={github_icon} width={35} alt="" /></a> */}
+      </div>
+      <p>Crafted with ❤️ in India by Aashish Gulshan.
+      </p>
+      No copyright—feel free to copy! Ping me if you need any help
+
     </div>
   )
 }
